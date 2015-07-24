edfb18e5acae5893f385063e3a8589c97c008c74
Nolawee Mengist <nolawee.mengist@BOUMODMAC001.local>
2015-07-24 15:48:56 -0600

    Added Categories

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var multer = require('multer');
 var flash = require('connect-flash');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var posts = require('./routes/posts');
 
 var app = express();
 
@@ -23,7 +23,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // Handle File Uploads & Multipart Data
-var upload = multer({ dest: './public/images/uploads'});
+app.use(multer({ dest: './public/images/uploads'}));
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -73,7 +73,7 @@ app.use(function(req,res,next){
 });
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/posts', posts);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
